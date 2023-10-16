30513d3fee03876b5e632f8d7f3a9436762f3f5c
Dmitry Litvinenko <ds-sev@ya.ru>
2023-10-16 20:48:09 +0300

    feat: split css to blocks

--- src/script.js
+++ src/script.js
@@ -1,10 +1,53 @@
-console.log('ljkjk')
+const languageButtonElement = document.querySelector('#langButton')
+const languageButtonMobileElement = document.querySelector('#langButtonMobile')
 
-const blogElement = document.querySelector('.blog')
 
-blogElement.addEventListener('click', () => console.log('XYZ'))
 
-blogElement.scrollTop = 30;
-blogElement.scrollLeft = -130;
+const languageButtonTextElements = document.querySelectorAll('.header__button-text')
 
-console.log(blogElement.scroll)
+const burgerButtonElement = document.querySelector('.header__burger-button')
+const mobileMenuElement = document.querySelector('.header__nav-menu-container')
+
+
+languageButtonElement.addEventListener('click', () => {
+  changeLang()
+})
+
+languageButtonMobileElement.addEventListener('click', () => {
+  changeLang()
+})
+
+const changeLang = () => {
+  languageButtonTextElements.forEach((languageButtonTextElement) => {
+    languageButtonTextElement.classList.toggle('header__button-text_hidden')
+  })
+}
+
+
+
+
+burgerButtonElement.addEventListener('click', () => {
+  mobileMenuElement.classList.toggle('header__nav-menu-container_visible')
+  burgerButtonElement.classList.toggle('header__burger-button_type_close')
+  // scrollController.disableScroll()
+  if (mobileMenuElement.classList.contains('header__nav-menu-container_visible')) {
+    scrollController.disableScroll()
+  } else {
+    scrollController.enableScroll()
+  }
+
+
+})
+
+const handleMenuButtonClick = () => {
+
+}
+
+const scrollController = {
+  disableScroll() {
+    document.body.style.cssText = `overflow: hidden; height: 100%`
+  },
+  enableScroll() {
+    document.body.style.cssText = ''
+  }
+}
